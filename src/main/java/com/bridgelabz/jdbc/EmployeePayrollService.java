b3dbb2a20a8443b75c420226060cfe9cba5d518c
Rushiraj11 <grushiraj95@gmail.com>
2021-10-03 18:24:57 +0530

    Added:Connection to Database

--- src/main/java/com/bridgelabz/jdbc/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayrollService.java
@@ -1,7 +1,27 @@
 package com.bridgelabz.jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class EmployeePayrollService {
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeePayroll Service JDBC program");
+        getConnection();
+    }
+
+    private static Connection getConnection() {
+        String url = "jdbc:mysql://localhost/employeepayroll_service";
+        String username = "root";
+        String password = "rushiraj@123456789";
+        Connection connection = null;
+        try {
+              // load and register the driver
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection Established");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return connection;
     }
 }

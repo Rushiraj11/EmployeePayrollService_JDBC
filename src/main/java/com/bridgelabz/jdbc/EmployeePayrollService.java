9fe97199a1069229c9c6526df6c201aae827fd49
Rushiraj11 <grushiraj95@gmail.com>
2021-10-03 17:40:46 +0530

    Added:Welcome Message

---
+++ src/main/java/com/bridgelabz/jdbc/EmployeePayrollService.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.jdbc;
+
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeePayroll Service JDBC program");
+    }
+}
